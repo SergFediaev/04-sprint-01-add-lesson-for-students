c5b441799b5f600299f788c20d2240279e645c9b
SergFediaev <sergfediaev@gmail.com>
2024-05-10 06:14:36 +0300

    5. Reset-form

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -2,6 +2,8 @@ import { AddDeckParams, decksApi } from './decks-api.ts'
 import { addDeckAC, setDecks } from './decks-reducer.ts'
 import { AppDispatch } from '../../app/store.ts'
 
-export const fetchDecks = () => (dispatch: AppDispatch) => decksApi.fetchDecks().then(response => dispatch(setDecks(response.data.items)))
+export const fetchDecks = () => (dispatch: AppDispatch) => {
+  decksApi.fetchDecks().then(response => dispatch(setDecks(response.data.items)))
+}
 
 export const addDeckTC = (params: AddDeckParams) => (dispatch: AppDispatch) => decksApi.addDeck(params).then(response => dispatch(addDeckAC(response.data)))
